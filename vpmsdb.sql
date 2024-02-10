2d61de1179bc49c41199c2df7baecf43d404def6
Pratiksha_Arve <136780458+Pratu-Arve@users.noreply.github.com>
2024-02-10 22:13:19 +0530

    Add files via upload

---
+++ vpmsdb.sql
@@ -0,0 +1,188 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Oct 05, 2023 at 08:57 AM
+-- Server version: 10.4.22-MariaDB
+-- PHP Version: 8.1.1
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `vpmsdb`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbladmin`
+--
+
+CREATE TABLE `tbladmin` (
+  `ID` int(10) NOT NULL,
+  `AdminName` varchar(120) DEFAULT NULL,
+  `UserName` varchar(120) DEFAULT NULL,
+  `MobileNumber` bigint(10) DEFAULT NULL,
+  `Email` varchar(200) DEFAULT NULL,
+  `Password` varchar(120) DEFAULT NULL,
+  `AdminRegdate` timestamp NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `tbladmin`
+--
+
+INSERT INTO `tbladmin` (`ID`, `AdminName`, `UserName`, `MobileNumber`, `Email`, `Password`, `AdminRegdate`) VALUES
+(1, 'Admin', 'admin', 7898799798, 'tester1@gmail.com', 'f925916e2754e5e03f75dd58a5733251', '2019-07-05 05:38:23');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tblcategory`
+--
+
+CREATE TABLE `tblcategory` (
+  `ID` int(10) NOT NULL,
+  `VehicleCat` varchar(120) DEFAULT NULL,
+  `CreationDate` timestamp NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `tblcategory`
+--
+
+INSERT INTO `tblcategory` (`ID`, `VehicleCat`, `CreationDate`) VALUES
+(1, 'Four Wheeler Vehicle', '2022-05-01 11:06:50'),
+(2, 'Two Wheeler Vehicle', '2022-03-02 11:07:09'),
+(4, 'Bicycles', '2022-05-03 11:31:17'),
+(6, '5 wheeler', '2023-05-09 13:35:32'),
+(7, 'chhota hathi', '2023-06-14 08:41:32');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tblregusers`
+--
+
+CREATE TABLE `tblregusers` (
+  `ID` int(5) NOT NULL,
+  `FirstName` varchar(250) DEFAULT NULL,
+  `LastName` varchar(250) DEFAULT NULL,
+  `MobileNumber` bigint(10) DEFAULT NULL,
+  `Email` varchar(250) DEFAULT NULL,
+  `Password` varchar(250) DEFAULT NULL,
+  `RegDate` timestamp NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `tblregusers`
+--
+
+INSERT INTO `tblregusers` (`ID`, `FirstName`, `LastName`, `MobileNumber`, `Email`, `Password`, `RegDate`) VALUES
+(3, 'Chetan', 'Barange', 7666331959, 'chetanbarangecb@gmail.com', 'f91e15dbec69fc40f81f0876e7009648', '2023-05-03 17:08:28'),
+(4, 'Rahul ', 'Munne', 9876543210, 'rahulmunne@gmail.com', 'f91e15dbec69fc40f81f0876e7009648', '2023-06-10 06:40:23'),
+(6, 'chirayu', 'kulkarni', 7057255511, 'chirayuK1234@gmail.com', 'be2f67f77e2245eeab319a8aee231215', '2023-08-09 06:52:38'),
+(7, 'mayur', 'manik', 7777331959, 'mayurmanik@gmail.com', 'c4f4b2eb6d63dd4dd8afed001c61c956', '2023-08-09 07:45:24');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tblvehicle`
+--
+
+CREATE TABLE `tblvehicle` (
+  `ID` int(10) NOT NULL,
+  `ParkingNumber` varchar(120) DEFAULT NULL,
+  `VehicleCategory` varchar(120) NOT NULL,
+  `VehicleCompanyname` varchar(120) DEFAULT NULL,
+  `RegistrationNumber` varchar(120) DEFAULT NULL,
+  `OwnerName` varchar(120) DEFAULT NULL,
+  `OwnerContactNumber` bigint(10) DEFAULT NULL,
+  `InTime` timestamp NULL DEFAULT current_timestamp(),
+  `OutTime` timestamp NULL DEFAULT NULL ON UPDATE current_timestamp(),
+  `ParkingCharge` varchar(120) NOT NULL,
+  `Remark` mediumtext NOT NULL,
+  `Status` varchar(5) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `tblvehicle`
+--
+
+INSERT INTO `tblvehicle` (`ID`, `ParkingNumber`, `VehicleCategory`, `VehicleCompanyname`, `RegistrationNumber`, `OwnerName`, `OwnerContactNumber`, `InTime`, `OutTime`, `ParkingCharge`, `Remark`, `Status`) VALUES
+(9, '836763848', 'Four Wheeler Vehicle', 'Audi', 'MH12AN9578', 'Chetan Barange', 7666331959, '2023-05-03 17:39:56', '2023-05-03 17:40:49', '20', 'Done', 'Out'),
+(10, '198735313', 'chhota hathi', 'Tata', '877653536856753', 'Pawan Bhoyar', 7843544335, '2023-06-14 08:42:11', '2023-06-14 08:42:31', '50', 'nothing', 'Out'),
+(11, '237500930', 'Bicycles', 'Ranger', '1956', 'Chirayu', 9897385457, '2023-08-09 06:26:35', '2023-08-09 06:27:18', '50', 'She is Going ', 'Out'),
+(12, '895911570', 'Four Wheeler Vehicle', 'BMW', '765465354', 'Mayur', 8776453465, '2023-08-09 07:44:44', NULL, '', '', '');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `tbladmin`
+--
+ALTER TABLE `tbladmin`
+  ADD PRIMARY KEY (`ID`);
+
+--
+-- Indexes for table `tblcategory`
+--
+ALTER TABLE `tblcategory`
+  ADD PRIMARY KEY (`ID`),
+  ADD KEY `VehicleCat` (`VehicleCat`);
+
+--
+-- Indexes for table `tblregusers`
+--
+ALTER TABLE `tblregusers`
+  ADD PRIMARY KEY (`ID`),
+  ADD KEY `MobileNumber` (`MobileNumber`);
+
+--
+-- Indexes for table `tblvehicle`
+--
+ALTER TABLE `tblvehicle`
+  ADD PRIMARY KEY (`ID`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `tbladmin`
+--
+ALTER TABLE `tbladmin`
+  MODIFY `ID` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `tblcategory`
+--
+ALTER TABLE `tblcategory`
+  MODIFY `ID` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+
+--
+-- AUTO_INCREMENT for table `tblregusers`
+--
+ALTER TABLE `tblregusers`
+  MODIFY `ID` int(5) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+
+--
+-- AUTO_INCREMENT for table `tblvehicle`
+--
+ALTER TABLE `tblvehicle`
+  MODIFY `ID` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
